78445f0f64aef59f8dca6e62c3098c9162a63b1b
Kacper Lenczuk <kacper.lenczuk@synapse.com>
2020-03-31 09:27:22 +0200

    README edit

--- README.txt
+++ README.txt
@@ -3,7 +3,7 @@ This is simple test of login feature on 9gag.com
 To run this test you can:
 
 1) open it in Eclipse and then run /Login9gag/src/test/java/Login9gag/Runner/MainRunner.java as JUnit test
-2)???
+2) if you have Maven installed: run cmd.exe and go to project directory, then run this command: mvn clean test -Dtest=Login9gag.Runner.MainRunner.java
 
 Test is checking couple of basic configuration of invalid data and one for valid email and password. Before running test with valid data, please fill columns in second table in /Login9gag/src/test/java/Login9gag/FeatureFiles/Login.feature with email and password to existing 9gag account.
 
--- report/cucumber/report.js
+++ report/cucumber/report.js
@@ -61,16 +61,48 @@ formatter.examples({
     {
       "cells": [
         "",
-        "Klenio12@"
+        "Example12@"
       ],
       "line": 16,
       "id": "login-into-9gag-account;login-into-account-with-invalid-user-data;;2"
+    },
+    {
+      "cells": [
+        "@gmail.com",
+        "Example12@"
+      ],
+      "line": 17,
+      "id": "login-into-9gag-account;login-into-account-with-invalid-user-data;;3"
+    },
+    {
+      "cells": [
+        "example",
+        "Example12@"
+      ],
+      "line": 18,
+      "id": "login-into-9gag-account;login-into-account-with-invalid-user-data;;4"
+    },
+    {
+      "cells": [
+        "example@gmail.com",
+        ""
+      ],
+      "line": 19,
+      "id": "login-into-9gag-account;login-into-account-with-invalid-user-data;;5"
+    },
+    {
+      "cells": [
+        "example@gmail.com",
+        "Example12@"
+      ],
+      "line": 20,
+      "id": "login-into-9gag-account;login-into-account-with-invalid-user-data;;6"
     }
   ],
   "keyword": "Examples"
 });
 formatter.before({
-  "duration": 13365464400,
+  "duration": 11806328300,
   "status": "passed"
 });
 formatter.scenario({
@@ -101,7 +133,7 @@ formatter.step({
 });
 formatter.step({
   "line": 9,
-  "name": "User enters a password: Klenio12@",
+  "name": "User enters a password: Example12@",
   "matchedColumns": [
     1
   ],
@@ -121,14 +153,14 @@ formatter.match({
   "location": "Login.user_navigates_to_website()"
 });
 formatter.result({
-  "duration": 1828561900,
+  "duration": 2494658600,
   "status": "passed"
 });
 formatter.match({
   "location": "Login.user_closes_the_pop_up_message()"
 });
 formatter.result({
-  "duration": 2174360000,
+  "duration": 2179234600,
   "status": "passed"
 });
 formatter.match({
@@ -141,38 +173,446 @@ formatter.match({
   "location": "Login.user_enters_a_username(String)"
 });
 formatter.result({
-  "duration": 169143300,
+  "duration": 104869300,
   "status": "passed"
 });
 formatter.match({
   "arguments": [
     {
-      "val": " Klenio12@",
+      "val": " Example12@",
       "offset": 23
     }
   ],
   "location": "Login.user_enters_a_password(String)"
 });
 formatter.result({
-  "duration": 242869800,
+  "duration": 226080500,
   "status": "passed"
 });
 formatter.match({
   "location": "Login.user_clicks_on_the_login_button()"
 });
 formatter.result({
-  "duration": 1618921000,
+  "duration": 1182770900,
   "status": "passed"
 });
 formatter.match({
   "location": "Login.user_should_be_taken_to_unsuccessful_login_page()"
 });
 formatter.result({
-  "duration": 178888100,
+  "duration": 133946600,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 875400,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 17,
+  "name": "Login into account with invalid user data",
+  "description": "",
+  "id": "login-into-9gag-account;login-into-account-with-invalid-user-data;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 6,
+  "name": "User navigates to 9gag login website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "User closes the pop up message",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "User enters a username: @gmail.com",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "User enters a password: Example12@",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "User clicks on the login button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 11,
+  "name": "User should be taken to unsuccessful login page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Login.user_navigates_to_website()"
+});
+formatter.result({
+  "duration": 1423047400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Login.user_closes_the_pop_up_message()"
+});
+formatter.result({
+  "duration": 2031690400,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": " @gmail.com",
+      "offset": 23
+    }
+  ],
+  "location": "Login.user_enters_a_username(String)"
+});
+formatter.result({
+  "duration": 96847600,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": " Example12@",
+      "offset": 23
+    }
+  ],
+  "location": "Login.user_enters_a_password(String)"
+});
+formatter.result({
+  "duration": 117529500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Login.user_clicks_on_the_login_button()"
+});
+formatter.result({
+  "duration": 1450459200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Login.user_should_be_taken_to_unsuccessful_login_page()"
+});
+formatter.result({
+  "duration": 50718000,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 488300,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 18,
+  "name": "Login into account with invalid user data",
+  "description": "",
+  "id": "login-into-9gag-account;login-into-account-with-invalid-user-data;;4",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 6,
+  "name": "User navigates to 9gag login website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "User closes the pop up message",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "User enters a username: example",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "User enters a password: Example12@",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "User clicks on the login button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 11,
+  "name": "User should be taken to unsuccessful login page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Login.user_navigates_to_website()"
+});
+formatter.result({
+  "duration": 1985968600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Login.user_closes_the_pop_up_message()"
+});
+formatter.result({
+  "duration": 2025662000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": " example",
+      "offset": 23
+    }
+  ],
+  "location": "Login.user_enters_a_username(String)"
+});
+formatter.result({
+  "duration": 83239700,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": " Example12@",
+      "offset": 23
+    }
+  ],
+  "location": "Login.user_enters_a_password(String)"
+});
+formatter.result({
+  "duration": 115675600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Login.user_clicks_on_the_login_button()"
+});
+formatter.result({
+  "duration": 1437640500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Login.user_should_be_taken_to_unsuccessful_login_page()"
+});
+formatter.result({
+  "duration": 93925100,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 613000,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 19,
+  "name": "Login into account with invalid user data",
+  "description": "",
+  "id": "login-into-9gag-account;login-into-account-with-invalid-user-data;;5",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 6,
+  "name": "User navigates to 9gag login website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "User closes the pop up message",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "User enters a username: example@gmail.com",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "User enters a password: ",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "User clicks on the login button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 11,
+  "name": "User should be taken to unsuccessful login page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Login.user_navigates_to_website()"
+});
+formatter.result({
+  "duration": 1645548200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Login.user_closes_the_pop_up_message()"
+});
+formatter.result({
+  "duration": 2039916700,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": " example@gmail.com",
+      "offset": 23
+    }
+  ],
+  "location": "Login.user_enters_a_username(String)"
+});
+formatter.result({
+  "duration": 126281600,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": " ",
+      "offset": 23
+    }
+  ],
+  "location": "Login.user_enters_a_password(String)"
+});
+formatter.result({
+  "duration": 81109100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Login.user_clicks_on_the_login_button()"
+});
+formatter.result({
+  "duration": 1476740400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Login.user_should_be_taken_to_unsuccessful_login_page()"
+});
+formatter.result({
+  "duration": 216722200,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 2320900,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 20,
+  "name": "Login into account with invalid user data",
+  "description": "",
+  "id": "login-into-9gag-account;login-into-account-with-invalid-user-data;;6",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 6,
+  "name": "User navigates to 9gag login website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "User closes the pop up message",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "User enters a username: example@gmail.com",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "User enters a password: Example12@",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "User clicks on the login button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 11,
+  "name": "User should be taken to unsuccessful login page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Login.user_navigates_to_website()"
+});
+formatter.result({
+  "duration": 772399600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Login.user_closes_the_pop_up_message()"
+});
+formatter.result({
+  "duration": 2033067300,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": " example@gmail.com",
+      "offset": 23
+    }
+  ],
+  "location": "Login.user_enters_a_username(String)"
+});
+formatter.result({
+  "duration": 113369000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": " Example12@",
+      "offset": 23
+    }
+  ],
+  "location": "Login.user_enters_a_password(String)"
+});
+formatter.result({
+  "duration": 118711900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Login.user_clicks_on_the_login_button()"
+});
+formatter.result({
+  "duration": 1431235900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Login.user_should_be_taken_to_unsuccessful_login_page()"
+});
+formatter.result({
+  "duration": 61968500,
   "status": "passed"
 });
 formatter.scenarioOutline({
-  "line": 19,
+  "line": 22,
   "name": "Login into account with valid user data",
   "description": "",
   "id": "login-into-9gag-account;login-into-account-with-valid-user-data",
@@ -180,37 +620,37 @@ formatter.scenarioOutline({
   "keyword": "Scenario Outline"
 });
 formatter.step({
-  "line": 21,
+  "line": 24,
   "name": "User navigates to 9gag login website",
   "keyword": "Given "
 });
 formatter.step({
-  "line": 22,
+  "line": 25,
   "name": "User closes the pop up message",
   "keyword": "And "
 });
 formatter.step({
-  "line": 23,
+  "line": 26,
   "name": "User enters a username: \u003cValidUsername\u003e",
   "keyword": "And "
 });
 formatter.step({
-  "line": 24,
+  "line": 27,
   "name": "User enters a password: \u003cValidPassword\u003e",
   "keyword": "And "
 });
 formatter.step({
-  "line": 25,
+  "line": 28,
   "name": "User clicks on the login button",
   "keyword": "When "
 });
 formatter.step({
-  "line": 26,
+  "line": 29,
   "name": "User should be taken to successful login page",
   "keyword": "Then "
 });
 formatter.examples({
-  "line": 28,
+  "line": 31,
   "name": "",
   "description": "",
   "id": "login-into-9gag-account;login-into-account-with-valid-user-data;",
@@ -220,26 +660,26 @@ formatter.examples({
         "ValidUsername",
         "ValidPassword"
       ],
-      "line": 29,
+      "line": 32,
       "id": "login-into-9gag-account;login-into-account-with-valid-user-data;;1"
     },
     {
       "cells": [
-        "wasylion@gmail.com",
-        "Klenio12@"
+        "example@gmail.com",
+        "Example12@"
       ],
-      "line": 30,
+      "line": 33,
       "id": "login-into-9gag-account;login-into-account-with-valid-user-data;;2"
     }
   ],
   "keyword": "Examples"
 });
 formatter.before({
-  "duration": 770200,
+  "duration": 548100,
   "status": "passed"
 });
 formatter.scenario({
-  "line": 30,
+  "line": 33,
   "name": "Login into account with valid user data",
   "description": "",
   "id": "login-into-9gag-account;login-into-account-with-valid-user-data;;2",
@@ -247,38 +687,38 @@ formatter.scenario({
   "keyword": "Scenario Outline"
 });
 formatter.step({
-  "line": 21,
+  "line": 24,
   "name": "User navigates to 9gag login website",
   "keyword": "Given "
 });
 formatter.step({
-  "line": 22,
+  "line": 25,
   "name": "User closes the pop up message",
   "keyword": "And "
 });
 formatter.step({
-  "line": 23,
-  "name": "User enters a username: wasylion@gmail.com",
+  "line": 26,
+  "name": "User enters a username: example@gmail.com",
   "matchedColumns": [
     0
   ],
   "keyword": "And "
 });
 formatter.step({
-  "line": 24,
-  "name": "User enters a password: Klenio12@",
+  "line": 27,
+  "name": "User enters a password: Example12@",
   "matchedColumns": [
     1
   ],
   "keyword": "And "
 });
 formatter.step({
-  "line": 25,
+  "line": 28,
   "name": "User clicks on the login button",
   "keyword": "When "
 });
 formatter.step({
-  "line": 26,
+  "line": 29,
   "name": "User should be taken to successful login page",
   "keyword": "Then "
 });
@@ -286,55 +726,55 @@ formatter.match({
   "location": "Login.user_navigates_to_website()"
 });
 formatter.result({
-  "duration": 1089416200,
+  "duration": 1216248200,
   "status": "passed"
 });
 formatter.match({
   "location": "Login.user_closes_the_pop_up_message()"
 });
 formatter.result({
-  "duration": 2059168000,
+  "duration": 2035260200,
   "status": "passed"
 });
 formatter.match({
   "arguments": [
     {
-      "val": " wasylion@gmail.com",
+      "val": " example@gmail.com",
       "offset": 23
     }
   ],
   "location": "Login.user_enters_a_username(String)"
 });
 formatter.result({
-  "duration": 174960500,
+  "duration": 106739400,
   "status": "passed"
 });
 formatter.match({
   "arguments": [
     {
-      "val": " Klenio12@",
+      "val": " Example12@",
       "offset": 23
     }
   ],
   "location": "Login.user_enters_a_password(String)"
 });
 formatter.result({
-  "duration": 118925000,
+  "duration": 104621400,
   "status": "passed"
 });
 formatter.match({
   "location": "Login.user_clicks_on_the_login_button()"
 });
 formatter.result({
-  "duration": 1646340800,
+  "duration": 1640232200,
   "status": "passed"
 });
 formatter.match({
   "location": "Login.user_should_be_taken_to_successful_login_page()"
 });
 formatter.result({
-  "duration": 135394900,
-  "error_message": "java.lang.AssertionError: expected [false] but found [true]\r\n\tat org.testng.Assert.fail(Assert.java:94)\r\n\tat org.testng.Assert.failNotEquals(Assert.java:513)\r\n\tat org.testng.Assert.assertEqualsImpl(Assert.java:135)\r\n\tat org.testng.Assert.assertEquals(Assert.java:116)\r\n\tat org.testng.Assert.assertEquals(Assert.java:305)\r\n\tat org.testng.Assert.assertEquals(Assert.java:315)\r\n\tat Login9gag.StepsFiles.Login.user_should_be_taken_to_successful_login_page(Login.java:167)\r\n\tat ✽.Then User should be taken to successful login page(Login.feature:26)\r\n",
+  "duration": 41654800,
+  "error_message": "java.lang.AssertionError: expected [false] but found [true]\r\n\tat org.testng.Assert.fail(Assert.java:94)\r\n\tat org.testng.Assert.failNotEquals(Assert.java:513)\r\n\tat org.testng.Assert.assertEqualsImpl(Assert.java:135)\r\n\tat org.testng.Assert.assertEquals(Assert.java:116)\r\n\tat org.testng.Assert.assertEquals(Assert.java:305)\r\n\tat org.testng.Assert.assertEquals(Assert.java:315)\r\n\tat Login9gag.StepsFiles.Login.user_should_be_taken_to_successful_login_page(Login.java:167)\r\n\tat ✽.Then User should be taken to successful login page(Login.feature:29)\r\n",
   "status": "failed"
 });
 });
